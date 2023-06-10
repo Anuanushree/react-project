fb0f4699b03751ed89a1184aa36bd3f92841ce00
Anuanushree <anuabi2408@gmail.com>
2023-06-10 10:05:50 +0530

    create-htm-element

--- src/App.js
+++ src/App.js
@@ -1,39 +1,76 @@
-
-// function App() {
-//   const now = new Date();
-//   const a=10;
-//   const b=20;
-//   console.log(now,a+b)
-//   // console.log("hellow from component")
-//   return (
-//     <div>
-//       <p>Hello World.{now.toString()}</p>
-//       <p>
-//         {a}plus{b} is{a+b}
-//       </p>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react'
-
-function Hello(props) {
+function ListRight(props) {
   return (
-    <div>
-      <p>Hello {props.name},your age is {props.age}</p>
-    </div>
+    <li>{props.name}</li>
   )
 }
-
+function Btn(props) {
+  return (
+    <a href="#">{props.name}</a>
+  )
+}
+function ListWrong(props) {
+  return (
+    <li>{props.name}</li>
+  )
+}
 function App() {
-  const age = 10;
   return (
     <div>
-   <h2>Greeting</h2>
-   <Hello name='anu' age={10+20}/>
-    <Hello name='shree' age={age}/>
+      <div>
+        <h5> FREE</h5>
+        <h6> $0 month</h6>
+        <hr></hr>
+        <ul>
+          <ListRight name='Single User' />
+          <ListRight name='5GB Storage' />
+          <ListRight name='Unlimited Public Projects' />
+          <ListRight name='Community Access' />
+          <ListWrong name='Unlimited Private Projects' />
+          <ListWrong name='Dedicated Phone Support' />
+          <ListWrong name='Free Subdomain' />
+          <ListWrong name='Monthly Status Reports' />
+        </ul>
+        <div>
+          <Btn name="Button" />
+        </div>
+      </div>
+      <div>
+        <h5> PLUS</h5>
+        <h6> $9 month</h6>
+        <hr></hr>
+        <ul>
+          <ListRight name='5 Users' />
+          <ListRight name='50GB Storage' />
+          <ListRight name='Unlimited Public Projects' />
+          <ListRight name='Community Access' />
+          <ListRight name='Unlimited Private Projects' />
+          <ListRight name='Dedicated Phone Support' />
+          <ListRight name='Free Subdomain' />
+          <ListWrong name='Monthly Status Reports' />
+        </ul>
+        <div>
+          <Btn name="button" />
+        </div>
+      </div>
+      <div>
+        <h5> PRO</h5>
+        <h6>$49 month</h6>
+        <hr></hr>
+        <ul>
+          <ListRight name='5 Users' />
+          <ListRight name='150GB Storage' />
+          <ListRight name='Unlimited Public Projects' />
+          <ListRight name='Community Access' />
+          <ListRight name='Unlimited Private Projects' />
+          <ListRight name='Dedicated Phone Support' />
+          <ListRight name='Unlimited Free Subdomain' />
+          <ListRight name='Monthly Status Reports' />
+        </ul>
+        <div>
+          <Btn name="button" />
+        </div>
+      </div>
     </div>
   )
 }
