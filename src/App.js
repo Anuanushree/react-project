c763babd345c9ef46e8bf7f38a7372e3f935880a
Anuanushree <anuabi2408@gmail.com>
2023-06-16 15:38:56 +0530

    fetch data

--- src/App.js
+++ src/App.js
@@ -98,28 +98,112 @@
 // }
 
 // export default App
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 
-function App() {
-  const [counter, setcounter] = useState(0);
+// function App() {
+//   const [counter, setcounter] = useState(0);
 
-  const handleplus = () => {
-    setcounter(counter + 1)
-  }
-  const handleminus = () => {
-    setcounter(counter - 1)
-  }
-  const handlereset = () => {
-    setcounter(0)
-  }
-  return (
-    <div>
-      <div>{counter}</div>
-      <button onClick={handleplus}>plus</button>
-      <button onClick={handleminus}>minus</button>
-      <button onClick={handlereset}>reset</button>
-    </div>
-  )
-}
+//   const handleplus = () => {
+//     setcounter(counter + 1)
+//   }
+//   const handleminus = () => {
+//     setcounter(counter - 1)
+//   }
+//   const handlereset = () => {
+//     setcounter(0)
+//   }
+//   return (
+//     <div>
+//       <div>{counter}</div>
+//       <button onClick={handleplus}>plus</button>
+//       <button onClick={handleminus}>minus</button>
+//       <button onClick={handlereset}>reset</button>
+//     </div>
+//   )
+// }
 
-export default App
+// export default App
+// import React from 'react'
+// function Color(props) {
+//   return (
+//     <div>
+//       <p>{props.text}</p>
+//       <p>{props.code}</p>
+//     </div>
+//   )
+// }
+// function Card(props) {
+//   return (
+//     <div>
+//       <p>{props.text}</p>
+//       <p>{props.salary}</p>
+//       <p>{props.img}</p>
+//     </div>
+//   )
+// }
+// function App() {
+//   const monthly = "EARNINGS (MONTHLY)";
+//   const monthlysalary = '$ 40,000';
+//   // const img ='https://w7.pngwing.com/pngs/563/791/png-transparent-dollar-sign-illustration-united-states-dollar-icon-design-icon-dollar-sign-text-logo-number-thumbnail.png'
+//   const annual = "EARNINGS (ANNUAL)";
+//   const annualsalary = '$215,000';
+//   const task = "TASKS";
+//   const percentage = '50%';
+//   const request = "PENDING REQUESTS";
+//   const number = '18';
+//   return (
+//     <div>
+//       <div>
+//         <Card text={monthly} salary={monthlysalary} />
+//         <Card text={annual} salary={annualsalary} />
+//         <Card text={task} salary={percentage} />
+//         <Card text={request} salary={number} />
+//       </div>
+//       <div>
+//         <Color text='primary' code='#4e73df' />
+//         <Color text='Success' code='#1cc88a' />
+//         <Color text='Info' code='#36b9cc' />
+//         <Color text='Warning' code='#f6c23e' />
+//         <Color text='Danger' code='#e74a3b' />
+//         <Color text='Secondary' code='#858796' />
+//         <Color text='Ligh' code='#f8f9fc' />
+//         <Color text='Dark' code='#5a5c69' />
+
+//       </div>
+//     </div>
+
+//    )
+// }
+
+// export default App
+// import React from 'react'
+// import { useEffect, useState } from 'react'
+
+// function App() {
+//   const [data, setdata] = useState(null);
+
+//   useEffect(() => {
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//       .then((response) => response.json())
+//       .then((data) => setdata(data));
+
+//   }, []);
+//   return (
+//     <div>
+//       <h1>API DATA</h1>
+//       {
+//         data ? (
+//           < ul >
+//             {
+//               data.map(item => <li key={item.id}> {item.title} </li>)
+//             }
+//           </ul>
+//         ) : (
+//           <p>loading data....</p>
+//         )
+//       }
+//     </div >
+//   )
+// }
+
+// export default App
