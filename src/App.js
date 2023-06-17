091c1f8fc3774039f952fbad7e2c27fa1d167414
Anuanushree <anuabi2408@gmail.com>
2023-06-17 20:39:40 +0530

    react-task-3

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function Pagination(props) {
 function Color(props) {
   return (
     <>
-      <div className="col-lg-6 mb-4">
+      <div className="col-lg-6 col-md-6 mb-3">
         <div className={`card text-white shadow bg-${props.text}`}>
           <div className='card-body'>
             {props.text}
@@ -24,15 +24,13 @@ function Color(props) {
           </div>
         </div>
       </div>
-      {/* </div> */}
     </>
   )
 }
 function Card(props) {
   return (
     <>
-      {/* <div className="row"> */}
-      <div className="col-xl-3 col-md-4 mb-4" >
+      <div className="col-xl-3 col-md-6 mb-4 p-2" >
         <div className="card border-left-success shadow h-100 py-2">
           <div className="card-body">
             <div className="row align-items-center">
@@ -42,15 +40,12 @@ function Card(props) {
                 <div className="h5 mb-0 font-weight-bold text-gray-800">{props.salary}</div>
               </div>
               <div className="col-auto">
-                <img src={`${props.img}`} alt='...' />
+                <img src={`${props.img}`} style={{width:50}} alt='...' />
               </div>
             </div>
           </div>
         </div>
       </div>
-      {/* <p>{props.text}</p> */}
-      {/* <p>{props.salary}</p> */}
-      {/* <p>{props.img}</p> */}
     </ >
   )
 }
@@ -68,7 +63,8 @@ function App() {
   const number = '18';
   const requestimg = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAHoAAAB7CAMAAACM22RzAAAAYFBMVEX///8AAAB+fn5sbGx7e3sbGxsyMjK3t7c6OjpYWFjf39/t7e2FhYUtLS3MzMy9vb3GxsYJCQkjIyOjo6NMTEzz8/PW1taMjIxjY2OxsbH5+fmqqqrm5uZHR0eTk5NSUlJijh/AAAACKUlEQVRoge3ba5OCIBQGYFBLu0mZuWZq//9fbq0XDFwUgePuzHk/OZP2pKIDHCLkJ8yLLhQkl8hjhIclMGyX07mTv2Dhd+pGruHl1j6vIVMav+jTOvR2tZN+n3bfxgIPJEH3ENfk2W5lBChxC+akfaR9KJmQvBEjsmkvNxztN+IWaaSRRhpppJFGGul/Qd+ryDj5SB97mj5SK4n16b0dmSb69NUSTQ/r0TdturQkV/oXnDysyKf9AprE/s44tXSn//orBWmkkUYaaaSRnksXh0UpzOlHelqU9GpKGxSCajP6tlwe6QBr0UblGDZGzqYzE1oeaOnQxKDOuVHJM+hssb1RV49mPFyH63FRriPdfk3aVZBGGmmkkUYaaaSRRhrpFemo2Yim5gKspWjFtPsN1M/2Yn4bwJTSnhrJOjBQFXaS4xic7ews8b2r61kjLcBo3D3M65oGE587kr3Xl91Ua1jFmQlrq06bZl0qbIFmtuRt2bbYfCZ9tyXn/Eay3xamMwfyJfo8oYyxkId183XDnUJ+9HG4s14YkwusH4llelDQnpiJNItMD+YtHy5lmR4s2r87lSXa43LoVhbpAE4WaJ/LyolX+/TgjXOeOtAuveMvA+WMr3W6eHIZZNF+Txcpv9olhNzTIZcvMHJPU3BZphNlJcUlHYHJIl3ByQJdgY0PRPoJKX/QO1B5SAP++0egPWCZDzJGh11Oc1tN7vqfa8ivXndFU7edoW+hGDRzCevbIgAAAABJRU5ErkJggg=='
   return (
-    <div>
+    <div className='container-fluid'>
+      <h1 className='text-center'>Components</h1>
       <div>
         <div className='row '>
           <Card text={monthly} img={monthlyimg} salary={monthlysalary} />
@@ -79,23 +75,28 @@ function App() {
       </div>
       <div>
         <div className='row '>
-          <div className='col-6'>
-            <Color text='primary' code='#4e73df' />
-            <Color text='success' code='#1cc88a' />
-            <Color text='info' code='#36b9cc' />
-            <Color text='warning' code='#f6c23e' />
-            <Color text='danger' code='#e74a3b' />
-            <Color text='secondary' code='#858796' />
-            <Color style={{ color: 'red' }} text='ligh' code='#f8f9fc' />
-            <Color text='dark' code='#5a5c69' />
+          <div className='col-md-6'>
+            <div className='card'>
+              <div className='row'>
+                <Color text='primary' code='#4e73df' />
+                <Color text='success' code='#1cc88a' />
+                <Color text='info' code='#36b9cc' />
+                <Color text='warning' code='#f6c23e' />
+                <Color text='danger' code='#e74a3b' />
+                <Color text='secondary' code='#858796' />
+                <Color text='dark' code='#5a5c69' />
+              </div>
+            </div>
           </div>
-          <div className='col-6'>
-            <div class="card-body">
-              <Pagination text='Server Migration' per='20%' color='danger' />
-              <Pagination text='Sales Tracking ' per='40%' color='warning' />
-              <Pagination text='Customer Database' per='60%' color='secondary' />
-              <Pagination text='Payout Details' per='80%' color='info' />
-              <Pagination text='Account Setup' per='100%' color='success' />
+          <div className='col-md-6 '>
+            <div className='card'>
+              <div class="card-body ">
+                <Pagination text='Server Migration' per='20%' color='danger' />
+                <Pagination text='Sales Tracking ' per='40%' color='warning' />
+                <Pagination text='Customer Database' per='60%' color='secondary' />
+                <Pagination text='Payout Details' per='80%' color='info' />
+                <Pagination text='Account Setup' per='100%' color='success' />
+              </div>
             </div>
           </div>
         </div>
