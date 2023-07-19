3d95b753aa445749f01804a77e552974bf97d064
Anuanushree <anuabi2408@gmail.com>
2023-07-19 18:45:17 +0530

    random string

--- src/components/Rpassword.js
+++ src/components/Rpassword.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import axios from 'axios';
 
 function Rpassword() {
+
+    const [email, setEmail] = useState('')
+
+    const handlesubmit = async (event) => {
+        event.preventDefault();
+        console.log('btn clicked');
+        console.log(email)
+        try {
+            const response = await axios.post('http://localhost:3001/api/users/reset', { email });
+        } catch (error) {
+            console.error(error.response.data.error)
+        }
+    }
     return (
 
         <>
@@ -20,7 +34,7 @@ function Rpassword() {
                                     <div className="col-md-6 col-lg-7 d-flex align-items-center">
                                         <div className="card-body p-4 p-lg-5 text-black">
 
-                                            <form>
+                                            <form >
 
                                                 <div className="d-flex align-items-center mb-3 pb-1">
                                                     <i className="fas fa-cubes fa-2x me-3" style={{ color: "#ff6219" }}></i>
@@ -30,13 +44,19 @@ function Rpassword() {
                                                 <h2 className="fw-normal mb-3 pb-3" style={{ letterSpacing: "1px" }}>Forgot Password?</h2>
                                                 <h5 className="fw-normal mb-3 pb-3">You can reset your password here.</h5>
                                                 <div className="form-outline mb-4">
-                                                   
-                                                <input type="email" id="form2Example17" className="form-control form-control-lg" />
-                                                <label className="form-label" >Email address</label>
+
+                                                    <input type="email" id="form2Example17"
+
+                                                        value={email}
+                                                        onChange={(event) => setEmail(event.target.value)}
+                                                        className="form-control form-control-lg" />
+                                                    <label className="form-label" >Email address</label>
                                                 </div>
 
                                                 <div className="pt-1 mb-4">
-                                                    <button className="btn btn-dark btn-lg btn-block" type="button">Reset your password</button>
+                                                    <button className="btn btn-dark btn-lg btn-block"
+                                                        onClick={handlesubmit}
+                                                        type="button">Reset your password</button>
                                                 </div>
 
                                                 <a className="small text-muted">Terms of use.</a>
