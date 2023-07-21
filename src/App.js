390e2a8cf7b5bebf3c370b8a14624d4bc9d2a8af
Anuanushree <anuabi2408@gmail.com>
2023-07-21 19:45:09 +0530

    trial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React  from 'react'
-import { Route, BrowserRouter as Router, Routes, Link } from 'react-router-dom';
-import RegisterForm from './components/RegisterForm';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Rpassword from './components/Rpassword';
 import './App.css'
 import Thankyou from './components/thankyou';
 import Message from './components/message';
-import Register from './components/Register';
+
 
 
 function App() {
 
   return (
     <Router>
-      <Link to='/' ></Link>
       <Routes>
-        <Route path='/register/:resetToken' element={<RegisterForm />} />
+        {/* <Route path='/register/:resetToken' element={<RegisterForm />} /> */}
         <Route path='/' element={<Rpassword />} />
         <Route path='/thankyou' element={<Thankyou />} />
         <Route path='/message' element={<Message />} />
-        <Route path='/register' element={<Register />} />
+        {/* <Route path='/register' element={<Register />} /> */}
 
       </Routes>
 
